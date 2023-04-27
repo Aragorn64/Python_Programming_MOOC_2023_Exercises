e0deceea672b80dede80576d5e27ce25607d340a
hermito <hermito@localhost.localdomain>
2023-04-26 20:47:35 -0500

    Getting Started

---
+++ MOCC_2023/Part 1/Getting_Started/Getting_Started.py
@@ -0,0 +1,30 @@
+print("Hello there!")
+
+# 1. Programming exercise: Emoticon
+
+print(":-)")
+
+# 2. Programming exercise: Fix the code: Seven Brothers
+
+print("Aapo")
+print("Eero")
+print("Juhani")
+print("Lauri")
+print("Simeoni")
+print("Timo")
+print("Tuomas")
+
+# 3. Programming exercise: Row, Row, Row Your Boat
+
+print("Row, row, row your boat,")
+print("Gently down the stream.")
+print("Merrily, merrily, merrily, merrily,")
+print("Life is but a dream.")
+
+# 4. Programming exercise: Minutes in a year
+
+print(60*24*365)
+
+# 5. Programming exercise: Print some code
+
+print('print("Hello there!")')
